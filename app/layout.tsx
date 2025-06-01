cfa23d45041227ff861de0b30ddaadd130525073
Eluruchandrika <chandrikaeluru2005@gmail.com>
2025-06-01 23:41:34 +0530

    project updates and some animation changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ inject();
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://chetankesare9.vercel.app/"),
+	metadataBase: new URL("https://chaxfolio.vercel.app/"),
 
 	title: 'Chandrika Eluru | Portfolio',
 	authors: {
@@ -24,11 +24,11 @@ export const metadata: Metadata = {
 		description:
 			"Evolving Code, Self-Taught Spirit: Web Development Journey from India.",
 		url: "https://vercel.com/chandrikas-projects-252f764c",
-		siteName: "Chetan Kesare",
+		siteName: "Chandrika Eluru",
 		images: "/newPortfolio.png",
 		type: "website",
 	},
-	keywords: ["personal portfolio website", "portfolio website templete", "web developer", "web Development roadmap", "react portfolio website", "3d portfolio website", "three.js portfolio website", "portfolio website github","portfolio website template","free portfolio website", "chetan kesare", "chetan kesare github", "chetan kesare linkedin", "chetan kesare from sangli", "terna engineering college nerul", "terna college", "terna college mumbai", "terna dental college"],
+	keywords: ["personal portfolio website", "portfolio website templete", "web developer", "web Development roadmap", "react portfolio website", "3d portfolio website", "three.js portfolio website", "portfolio website github","portfolio website template","free portfolio website", "chandrika ELuru", "chandrika ELuru github", "chandrika ELuru linkedin"],
 };
 
 
